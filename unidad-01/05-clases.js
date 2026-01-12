70bbc9a3763a26777f0420ae66d78c1c72da29ef
Jonathan Cruz <cruzjonathan.e@gmail.com>
2026-01-12 20:46:48 -0300

    Unidad 01

--- unidad-01/05-clases.js
+++ unidad-01/05-clases.js
@@ -2,11 +2,13 @@ class Persona {
     // Atributos
     nombre = '';
     edad = 0;
+    static contadorGlobal = 0;
 
     constructor(nombre, edad){
         //console.log('Hola desde el contructor');
         this.nombre = nombre;
         this.edad = edad;
+        this.contador = 0;
     }
     // Methods
     saludar = ()=> {
@@ -21,8 +23,12 @@ class Persona {
     }
     incrementar = () => {
         this.contador++;
+        Persona.contadorGlobal++;
     }
-    
+    static getContadorGlobal = () => {
+        return Persona.contadorGlobal
+    }
+
 }
 
 const p1 = new Persona('José', 23);
@@ -34,3 +40,9 @@ p2.setEdad(30);
 console.log(`${p1.nombre} tiene ${ p1.edad } años`);
 console.log(`${p2.nombre} tiene ${ p2.getEdad() } años`);
 console.log( p1.edad);
+p1.incrementar();
+p2.incrementar();
+p1.incrementar();
+
+console.log(  p1.contador, p2.contador )
+console.log('Contador Global', Persona.getContadorGlobal());
