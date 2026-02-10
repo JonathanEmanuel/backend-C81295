0a1bd650576dc4a49e5f2998b52a599e48482191
Jonathan Cruz <cruzjonathan.e@gmail.com>
2026-02-09 21:34:29 -0300

    Unidad 05 - Motor de plantillas

--- unidad-05/src/app.js
+++ unidad-05/src/app.js
@@ -1,7 +1,12 @@
 import express from 'express'
 import handlebars from 'express-handlebars'
 import usersRouter from './routes/users.router.js'
+import path from 'path'
+import { fileURLToPath} from 'url'
+import { title } from 'process'
 
+const __filename = fileURLToPath( import.meta.url);
+const __dirname = path.dirname( __filename);
 
 const app = express();
 // Middleware -> Lo tratamos en las proximas clases
@@ -11,18 +16,28 @@ app.use( express.urlencoded({extended:true}) )
 const PORT = 8080;
 
 app.engine('handlebars', handlebars.engine() );
-app.set('views', '/views');
+app.set('views', path.join(__dirname, 'views' ) );
 app.set('view engine', 'handlebars');
 
 app.use( '/', express.static('public') );
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
+    const isAdmin = false;
     let data = {
         title: 'Home',
-        name: 'Lucas'
+        user: {
+            name: 'José',
+            email: 'jose@code.edu.ar',
+            isAdmin
+        },
+        products: [
+            { id: 1, title: '🍐', price: 20},
+            { id: 2, title: '🍎', price: 30 },
+            { id: 3, title: '🍊', price: 10 }
+        ]
     }
 
-    res.render('index, data')
+    res.render('index', data)
 })
 
 app.use('/api/users', usersRouter);
